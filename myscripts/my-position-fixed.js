164572fd828026df7c3759b27b29ef6a09acb307
Tika Natz <TikaNatz@gmail.com>
2019-10-11 22:19:14 -0700

    Final Draft

--- myscripts/my-position-fixed.js
+++ myscripts/my-position-fixed.js
@@ -1,6 +1,6 @@
 // Navigation
 const body = document.body;
-const btnShowNav = document.getElementById('nav-button');
+const btnShowNav = document.getElementById('menu');
 const navMain = document.getElementById('main-nav');
 btnShowNav.addEventListener('click', _ => { body.classList.toggle('show') });
 btnShowNav.addEventListener('mousedown', (e) => { e.preventDefault() });
